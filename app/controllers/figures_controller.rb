da066b5be9876528a22b92159885379b3a327766
Sara Stanton <sara.mpa13@gmail.com>
2018-06-24 07:24:13 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,44 @@
 class FiguresController < ApplicationController
 
+  #index action
+  get '/figures' do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+
+  #new action
+  get '/figures/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"/figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params[:name])
+    redirect to "/figures/#{@figure.id}"
+    binding.pry
+  end
+
+  #show action
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  #edit action
+  get '/figures/:id/edit' do
+    @figure = Figures.find(params[:id])
+    erb :edit
+  end
+
+  patch '/figures' do
+    @figure = Figure.find(params[:id])
+    @figure.name = params[:name]
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,40 @@
 class LandmarksController < ApplicationController
 
+
+    #index action
+    get '/landmarks' do
+      @landmarks = Landmark.all
+      erb :"/landmarks/index"
+    end
+
+    #new action
+    get '/landmarks/new' do
+      @landmarks = Landmark.all
+      erb :"/landmarks/new"
+    end
+
+    post '/landmarks' do
+      @landmark = Landmark.create(name: params[:name])
+      redirect to "/landmarks/#{@landmark.id}"
+    end
+
+    #show action
+    get '/landmarks/:id' do
+      @landmark = Landmark.find(params[:id])
+      erb :"landmarks/show"
+    end
+
+    #edit action
+    get '/landmarks/:id/edit' do
+      @landmark = Landmarks.find(params[:id])
+      erb :edit
+    end
+
+    patch '/landmarks' do
+      @landmark = Landmark.find(params[:id])
+      @landmark.name = params[:name]
+      @landmark.save
+      redirect to "/landmarks/#{@landmark.id}"
+    end
+
 end
