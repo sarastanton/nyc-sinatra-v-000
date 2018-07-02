532f098ec0f8accfa0b590503bea452a46cbeb74
Sara Stanton <sara.mpa13@gmail.com>
2018-07-02 05:40:15 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,39 +1,38 @@
 class LandmarksController < ApplicationController
 
+  #index action
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
 
-    #index action
-    get '/landmarks' do
-      @landmarks = Landmark.all
-      erb :"/landmarks/index"
-    end
+  #new action
+  get '/landmarks/new' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/new"
+  end
 
-    #new action
-    get '/landmarks/new' do
-      @landmarks = Landmark.all
-      erb :"/landmarks/new"
-    end
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:name])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 
-    post '/landmarks' do
-      @landmark = Landmark.create(name: params[:name])
-      redirect to "/landmarks/#{@landmark.id}"
-    end
+  #show action
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmarks/show"
+  end
 
-    #show action
-    get '/landmarks/:id' do
-      @landmark = Landmark.find(params[:id])
-      erb :"/landmarks/show"
-    end
+  #edit action
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmarks/edit"
+  end
 
-    #edit action
-    get '/landmarks/:id/edit' do
-      @landmark = Landmark.find(params[:id])
-      erb :"/landmarks/edit"
-    end
-
-    patch '/landmarks/:id' do
-      @landmark = Landmark.find_by_id(params[:id])
-      @landmark.update(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
-      redirect to "/landmarks/#{@landmark.id}"
-    end
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 
 end
